ce7c0e02017f012af031062fd004952586e0cafc
renoweebeloy-prog <renowee.beloy@dorsu.edu.ph>
2026-07-25 19:56:08 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,16 @@
 <?php
 session_start();
+
+// 1. I-delete ang tanang User Sessions
 session_unset();
 session_destroy();
+
+// 2. I-delete ang "Keep me signed in" Cookie (I-set ang oras pabalik sa past para ma-expire)
+if (isset($_COOKIE['cherryjoe_user'])) {
+    setcookie('cherryjoe_user', '', time() - 3600, "/"); 
+}
+
+// 3. I-kick out pabalik sa Login page
 header("Location: login.php");
 exit();
 ?>
