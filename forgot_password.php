ae2f7e85a5ed91b397246dfec3d0a4ca1bc28858
renoweebeloy-prog <renowee.beloy@dorsu.edu.ph>
2026-07-26 13:03:51 +0800

    Update forgot_password.php

--- forgot_password.php
+++ forgot_password.php
@@ -23,7 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             // ==========================================
             
             // BOSS: IBUTANG DINHI ANG WEB APP URL GIKAN SA GOOGLE APPS SCRIPT
-            $google_app_script_url = 'https://script.google.com/macros/s/AKfycbwgYTGMnmbubFoII6fCTyviOr9MMXUt8skwUGW0mGOcb7zVh_legtBKHo70L2bI8gwpvg/exec'; 
+            $google_app_script_url = 'https://script.google.com/macros/s/AKfycbxTDKLnoaOJGMm2Nd5nakoZONMiJAIdiX2jobng3HMUI5y_XEZcnxIo4bVuUG97N_YGqQ/exec'; 
 
             $html_content = "<div style='font-family: Arial, sans-serif; padding: 20px; background: #f4f4f4; border-radius: 10px;'>
                                 <h2 style='color: #059669;'>Password Reset Request</h2>
