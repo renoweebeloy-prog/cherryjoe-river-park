cf611d72e0a0d7396bbe14ae712a2e5fe2e4c1e7
renoweebeloy-prog <renowee.beloy@dorsu.edu.ph>
2026-07-31 20:53:28 +0800

    Update forgot_password.php

--- forgot_password.php
+++ forgot_password.php
@@ -23,7 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             // ==========================================
             // ILISI KINI SA IMONG BAG-ONG WEB APP URL
             // ==========================================
-            $google_app_script_url = 'https://script.google.com/macros/s/AKfycbw2tEl71Ge9W98MnMiNKMJ9UTa_huFJyIJ_Q6-0bwtJ-5NAzFBPbmDAgTs8yfZhxpr6Jg/exec'; 
+            $google_app_script_url = 'https://script.google.com/macros/s/AKfycbwH9Ok5JRHAU-mXY2svyqIx42zj5-eibBfod8EYL704gO7uFOJJ4O-RL4IuIOZqyzZHMg/exec'; 
 
             // Ipasa ang data gamit ang GET URL
             $url = $google_app_script_url . "?email=" . urlencode($email) . "&otp=" . $otp . "&name=" . urlencode($user['full_name']);
