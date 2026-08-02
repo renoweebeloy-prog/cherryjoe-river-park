e54b383923d85c68f71800d1a6ac4bdc722f551a
renoweebeloy-prog <renowee.beloy@dorsu.edu.ph>
2026-08-02 21:33:36 +0800

    Update google_login.php

--- google_login.php
+++ google_login.php
@@ -1,7 +1,3 @@
-<?php 
-if (session_status() === PHP_SESSION_NONE) { session_start(); }
-if (empty($_SESSION['admin_bypass'])) { header("Location: maintenance.php"); exit(); }
-
 ?>
 <?php
 session_start();
